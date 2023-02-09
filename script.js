51156395860bf0c74e02c987c9c5cf604cf2698a
Owen Lewis <owenalewis@gmail.com>
2023-02-09 17:20:18 +0000

    Refractors code to match new html setup Adds new private functions inside Game factory function. Removes old unused code elements.

--- script.js
+++ script.js
@@ -1,71 +1,98 @@
-const gameDisplay = document.querySelector("#gameDisplay");
-
-const gameBoard = (() => {
-  const array = ["X", "0", "", "0", "0", "X", "0", "X", "X"];
-  return { array };
-})();
-
-const displayController = (() => {
-  const updateDisplay = () => {
-    for (let i = 0; i < 9; i++) {
-      const div = document.querySelector(`#div${i}`);
-      div.textContent = `${gameBoard.array[i]}`;
-    }
-  };
-  return { updateDisplay };
-})();
+const X_CLASS = "x";
+const CIRC_CLASS = "circle";
+const WIN_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.getElementById("board");
+const winMessage = document.getElementById("winningMessage");
+const winText = document.querySelector("[data-winning-message-text]");
+const restartButton = document.getElementById("restartButton");
+let circTurn;
 
 const Game = (() => {
-  const initGame = () => {
-    for (let i = 0; i < 9; i++) {
-      const div = document.createElement("div");
-      div.setAttribute("id", `div${i}`);
-      div.textContent = "";
-      gameDisplay.appendChild(div);
+  const placeMark = (cell, currentClass) => {
+    cell.classList.add(currentClass);
+  };
+
+  const swapTurns = () => {
+    circTurn = !circTurn;
+  };
+
+  const setBoardHoverClass = () => {
+    board.classList.remove(X_CLASS);
+    board.classList.remove(CIRC_CLASS);
+    if (circTurn) {
+      board.classList.add(CIRC_CLASS);
+    } else {
+      board.classList.add(X_CLASS);
     }
   };
 
-  const checkEmpty = (sel) => {
-    const div = document.querySelector(`#div${sel}`);
-    let check = true;
-    if (div.textContent === "") {
-      check = true;
-    } else if (div.textContent === "X" || div.textContent === "0") {
-      check = false;
+  const endGame = (draw) => {
+    if (draw) {
+      winText.innerText = "Draw!";
+    } else {
+      winText.innerText = `${circTurn ? "0" : "X"} Wins!`;
     }
-    return check;
+    winMessage.classList.add("show");
   };
 
-  return { initGame, checkEmpty };
+  const isDraw = () =>
+    [...cellElements].every(
+      (cell) =>
+        cell.classList.contains(X_CLASS) || cell.classList.contains(CIRC_CLASS)
+    );
+
+  const checkWin = (currentClass) =>
+    WIN_COMBOS.some((combination) =>
+      combination.every((index) =>
+        cellElements[index].classList.contains(currentClass)
+      )
+    );
+
+  const handleMove = (e) => {
+    const cell = e.target;
+    const currentClass = circTurn ? CIRC_CLASS : X_CLASS;
+    placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+      endGame(false);
+    } else if (isDraw()) {
+      endGame(true);
+    } else {
+      swapTurns();
+      setBoardHoverClass();
+    }
+  };
+
+  const startGame = () => {
+    circTurn = false;
+    cellElements.forEach((cell) => {
+      cell.classList.remove(X_CLASS);
+      cell.classList.remove(CIRC_CLASS);
+      cell.removeEventListener("click", handleMove);
+      cell.addEventListener("click", handleMove, { once: true });
+    });
+    setBoardHoverClass();
+    winMessage.classList.remove("show");
+  };
+
+  startGame();
+
+  restartButton.addEventListener("click", (e) => {
+    e.stopPropagation();
+    startGame();
+  });
 })();
 
-const Player = (name, marker) => {
-  const makeMove = () => {
-    const move = prompt("Make your move... div#", " ");
-    if (Game.checkEmpty(move) === false) {
-      alert("Invalid selection! Make another selection");
-      makeMove();
-    } else if (Game.checkEmpty(move) === true) {
-      gameBoard.array[move] = marker;
-      displayController.updateDisplay();
-    }
-  };
+const Player = (name, marker) => ({ name, marker });
 
-  return { name, marker, makeMove };
-};
-
-Game.initGame();
-
-const player1 = Player("Player 1", "X");
-const player2 = Player("Player 0", "0");
-
-displayController.updateDisplay();
-
-/* PSUEDOCODE
-    - Players are assigned markers (either "X" or "0")
-    - Player 1 picks a position on gameBoard corresponding to particular div - [Player: makeMove()]
-    - Check whether div is empty - [Game: checkEmpty()]
-    - If true update gameBoard.array and call displayController.updateDisplay(), if false return error - [Game: handleMove()]
-    - Check for winner, if winner end game, if no winner continue game - [Game: checkForWinner()]
-    - Prompt next player's selection - [Game: changePlayer()]
-*/
+const player1 = Player("Player 1", X_CLASS);
+const player2 = Player("Player 2", CIRC_CLASS);
