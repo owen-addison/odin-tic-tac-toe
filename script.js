9f862202b0d5fa03bcd0851dbf5a56ab4cb5a16f
Owen Lewis <owenalewis@gmail.com>
2023-02-09 11:28:25 +0000

    Refactors code

--- script.js
+++ script.js
@@ -5,26 +5,35 @@ const gameBoard = (() => {
   return { array };
 })();
 
-const Player = (name, sign) => {
-  const sayHello = () => console.log("hello!");
-  return { name, sign };
-};
+const displayController = (() => {
+  const updateDisplay = () => {
+    for (let i = 0; i < 9; i++) {
+      const div = document.querySelector(`#div${i}`);
+      div.textContent = `${gameBoard.array[i]}`;
+    }
+  };
+  return { updateDisplay };
+})();
 
-const Game = () => {
+const Game = (() => {
   const initGame = () => {
-    // Create a div for each section of the board (9 total)
     for (let i = 0; i < 9; i++) {
       const div = document.createElement("div");
       div.setAttribute("id", `div${i}`);
-      div.textContent = `${gameBoard.array[i]}`;
+      div.textContent = " ";
       gameDisplay.appendChild(div);
     }
   };
 
   const nextTurn = 1;
   return { initGame, nextTurn };
+})();
+
+const Player = (name, sign) => {
+  const sayHello = () => console.log("hello!");
+  return { name, sign };
 };
 
-const game = Game();
+Game.initGame();
 
-game.initGame();
+displayController.updateDisplay();
